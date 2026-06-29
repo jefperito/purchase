6f09ad1d07c2e25f51cf34b2f667903d33a0a370
jeferson <jefperito@gmail.com>
2026-06-28 21:50:33 -0300

    fix: clean code

--- src/main/java/com/wexinc/purchasetransaction/service/PurchaseService.java
+++ src/main/java/com/wexinc/purchasetransaction/service/PurchaseService.java
@@ -1,20 +1,14 @@
 package com.wexinc.purchasetransaction.service;
 
 import com.wexinc.purchasetransaction.api.CreatePurchaseRequest;
-import com.wexinc.purchasetransaction.client.FiscalDataResponse;
-import com.wexinc.purchasetransaction.client.FiscalDataTreasuryApi;
-import com.wexinc.purchasetransaction.exception.CurrencyNotFoundException;
-import com.wexinc.purchasetransaction.exception.PurchaseNotFoundException;
 import com.wexinc.purchasetransaction.entity.Purchase;
+import com.wexinc.purchasetransaction.exception.PurchaseNotFoundException;
 import com.wexinc.purchasetransaction.repository.PurchaseRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.Comparator;
 import java.util.UUID;
 
 @Service
@@ -23,8 +17,6 @@ import java.util.UUID;
 public class PurchaseService {
 
     private final PurchaseRepository repository;
-
-    private final FiscalDataTreasuryApi dataTreasuryApi;
 
     public Purchase retrieve(final String purchaseId) {
         return repository.findById(UUID.fromString(purchaseId))
